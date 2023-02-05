c55abeec199c563fa482f342dc22402d91912a9a
amandadelu <amandadelu@gmail.com>
2023-02-04 19:48:40 -0700

    Update nurtients.lua
    nutrients initial commit

--- source/nurtients.lua
+++ source/nurtients.lua
@@ -1,1 +1,17 @@
 --object you can collide with, pickup, and increase nutrients count
+local pd <const> = playdate
+local gfx <const> = pd.graphics
+
+class('nurtients').extends(gfx.sprite)
+
+local nutrientCountSprite
+local nutrients
+
+
+function nurtients:init(x, y)
+local nutrientImage = gfx.image.new("images/Pando/Cells/Nutrients/Nutrients_Medium_01.png") 
+	nutrientSprite = gfx.sprite.new(nutrientImage)
+	nutrientSprite:setCollideRect(0,0,nutrientSprite:getSize())
+    nutrientSprite:moveTo(x, y)
+	nutrientSprite:add()
+end
